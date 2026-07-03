8307f980e0b2982695834de238b870cf17dfdf0f
Neha Geete <nehageete92@gmail.com>
2026-07-03 21:12:03 +0530

    Time: 10 ms (52.07%), Space: 59.6 MB (35.16%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -8,31 +8,46 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- import java.util.Arrays;
 class Solution {
-    public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null)
-        return head;
-        int count = 0; 
-        ListNode temp = head;
-        while(temp != null){ 
-            count ++; 
-            temp = temp.next; 
-        } 
-        int arr[] = new int[count]; 
-        temp = head;
-        for(int i=0;i<arr.length;i++){
-           arr[i] = temp.val;
-           temp = temp.next;
+   private ListNode findmiddle(ListNode head){
+    ListNode slow = head;
+    ListNode fast = head.next;
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+   }
+   
+   private ListNode merge2sortedll(ListNode lefthead, ListNode righthead){
+    ListNode t1 = lefthead;
+    ListNode t2 = righthead;
+    ListNode dummy = new ListNode(-1);
+    ListNode temp = dummy;
+    while(t1 != null && t2 != null){
+        if(t1.val < t2.val){
+            temp.next = t1;
+            temp = temp.next;
+            t1 = t1.next;
         }
-        Arrays.sort(arr);
-        temp = head;
-        int i = 0; 
-        while(temp != null){
-            temp.val = arr[i];
-             i++; 
-             temp = temp.next; 
-        } 
-        return head;
+        else {
+            temp.next = t2;
+            temp = temp.next;
+            t2 = t2.next;
+        }
+    }
+        if(t1 != null) temp.next = t1;
+        else temp.next = t2;
+        return dummy.next;
+   }
+
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode middle = findmiddle(head);
+        ListNode lefthead = head, righthead = middle.next;
+        middle.next = null;
+        lefthead = sortList(lefthead);
+        righthead = sortList(righthead);
+        return merge2sortedll(lefthead,righthead);
     }
 }
