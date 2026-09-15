8a0ba7c61775d6cf91b7b14aba2e24003a6355fe
Neha Geete <nehageete92@gmail.com>
2026-09-16 03:31:28 +0530

    Time: 3 ms (99.93%), Space: 202.3 MB (93.09%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -10,33 +10,12 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        if(head == null || head.next == null) return null; 
-        // THE BELOW CODE HAS TIME COMPLEXITY OF O(N+N/2)
-        // int count = 0;
-        // ListNode temp = head;
-        // while(temp!=null){
-        //     count ++;
-        //     temp = temp.next;
-        // }
-        // int ans = (count / 2);
-        // temp = head;
-        // while(temp != null){
-        //     ans--;
-        //     if(ans == 0) {
-        //     temp.next = temp.next.next;
-        //     break;
-        // }
-        // temp = temp.next;
-        // }
-        // return head;
-
-        // TORTOISE HARE SOLUTION 
-        ListNode slow = head;
-        ListNode fast = head;
+        if(head == null || head.next == null) return null;
+        ListNode slow = head, fast = head;
         fast = fast.next.next;
         while(fast != null && fast.next != null){
-            fast = fast.next.next;
             slow = slow.next;
+            fast = fast.next.next;
         }
         slow.next = slow.next.next;
         return head;
