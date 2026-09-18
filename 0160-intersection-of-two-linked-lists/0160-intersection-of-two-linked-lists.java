dc214d78e2ae12a1c50a9f3d417c06975a91ac18
Neha Geete <nehageete92@gmail.com>
2026-09-18 23:09:44 +0530

    Time: 2 ms (47.39%), Space: 53.1 MB (12.56%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,33 +11,16 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        // tc -> o(n*__)time for hashmap in black space sc-> o(n)
-        // HashMap<ListNode, Integer> map = new HashMap<>();
-        // if(headA == null || headB == null) return null;
-        // ListNode temp = headA;
-        // while(temp!=null){
-        //     map.put(temp,1);
-        //     temp = temp.next;
-        // }
-        // temp = headB;
-        // while(temp!=null){
-        //     if(map.containsKey(temp)){
-        //         return temp; 
-        //     }
-        //     temp = temp.next;
-        // }
-        // return null;
         if(headA == null || headB == null) return null;
-        ListNode temp1 = headA;
-        ListNode temp2 = headB;
-
-        while(temp1!= temp2){
-            temp1 = temp1.next;
-            temp2 = temp2.next;
-            if(temp1 == temp2) return temp1; // or temp2
-            if(temp1 == null) temp1 = headB;
-            if(temp2 == null) temp2 = headA;
-            }
-        return temp1; // or temp2
+        ListNode t1 = headA;
+        ListNode t2 = headB;
+        while(t1!= t2){
+            t1 = t1.next;
+            t2 = t2.next;
+            if(t1 == t2) return t1;
+            if(t1 == null) t1 = headB;
+            if(t2 == null) t2 = headA;
+        }
+        return t1;
     }
 }
